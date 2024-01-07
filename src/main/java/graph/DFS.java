fd8deccd3386c9fc90eb7acf9634735477b90a21
MohammadEhsan Akhavanpour <akhavanm@uwindsor.ca>
2024-01-07 07:12:00 -0500

    1557. Minimum Number of Vertices to Reach All Nodes.

--- src/main/java/graph/DFS.java
+++ src/main/java/graph/DFS.java
@@ -128,9 +128,75 @@ public class DFS {
         return String.valueOf(row) + "," + String.valueOf(col);
     }
 
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
+
+        for (int i = 0; i < rooms.size(); i++) {
+            graph.put(i, rooms.get(i));
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        visited.add(0);
+
+        while (!stack.isEmpty() && visited.size() < rooms.size()) {
+            int node = stack.pop();
+            for (int room : graph.get(node)) {
+                if (!visited.contains(room)) {
+                    visited.add(room);
+                    stack.add(room);
+                }
+            }
+        }
+        return visited.size() == rooms.size();
+    }
+
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        Set<Integer> ans = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            ans.add(i);
+            graph.put(i, new ArrayList<Integer>());
+        }
+
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (List<Integer> edge : edges) {
+            Integer x = edge.get(0);
+            Integer y = edge.get(1);
+            if (!graph.containsKey(x)) {
+                graph.put(x, new ArrayList<>());
+            }
+            graph.get(x).add(y);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(edges.get(0).get(0));
+
+        for (Integer key : graph.keySet()) {
+            stack.push(key);
+            while (!stack.isEmpty()) {
+                int curr = stack.pop();
+                List<Integer> childs = graph.get(curr);
+                if (childs != null) {
+                    for (Integer child : childs) {
+                        if (ans.contains(child)) {
+                            ans.remove(child);
+                            stack.push(child);
+                        }
+                    }
+                }
+            }
+        }
+        return ans.stream().toList();
+    }
+
     public static void main(String[] args) {
         DFS dfs = new DFS();
-        System.out.println(dfs.minReorder(6, new int[][]{{0, 1}, {1, 3}, {2, 3}, {4, 0}, {4, 5}}));
+        System.out.println(dfs.findSmallestSetOfVertices(6,
+                List.of(List.of(0, 1), List.of(0, 2),
+                        List.of(2, 5), List.of(3, 4),
+                        List.of(4, 2))));
     }
 
 }
